e172da2039442e6f290d5a672e58dc04f275e4ea
Michael Owuor <michael.owuor@gmail.com>
2023-07-27 22:32:44 +0300

    feat: create path aliases

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ function App() {
 	return (
 		<div className="p-8 text-center">
 			<h1 className="text-4xl font-bold text-blue-800 tracking-wide">
-				Your settings are well configured
+				Your app settings are well configured
 			</h1>
 			<p className="mt-5">
 				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, cum
